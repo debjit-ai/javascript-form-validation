bbd4dc2952c5714dfceaf135103179b91e7d7e03
debjit-ai <80821263+debjit-ai@users.noreply.github.com>
2022-05-12 22:06:08 +0530

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,93 @@
+// JavaScript Document
+let zip=document.getElementById("textZip Code");
+let add=document.getElementById("textAddress");
+let fullname=document.getElementById("textFull Name");
+let userName=document.getElementById("txtUserName");
+let email=document.getElementById("txtEmail");
+let pwd=document.getElementById("txtPwd");
+let conPwd=document.getElementById("txtConPwd");
+let form=document.querySelector("form");
+
+function validateInput(){
+    //check username is empty 
+    if(userName.value.trim()===""){
+       onError(userName,"User Name cannot be empty");
+    }else{
+        onSuccess(userName);
+    }
+    if(email.value.trim()===""){
+        onError(email,"Email cannot be empty");
+    }else{
+        if(!isValidEmail(email.value.trim())){
+            onError(email,"Email is not valid");
+        }else{
+            onSuccess(email);
+        }
+    }
+
+    //password
+    if(pwd.value.trim()===""){
+        onError(pwd,"Password cannot be empty");
+     }else{
+         onSuccess(pwd);
+     }
+     if(conPwd.value.trim()===""){
+        onError(conPwd,"Confirm Password  cannot be empty");
+     }else{
+         if(pwd.value.trim()!==conPwd.value.trim()){
+            onError(conPwd,"Password & Confirm password not matching");
+         }
+         else
+         onSuccess(conPwd);
+     }
+	// Full Name
+	if(fullname.value.trim()===""){
+        onError(fullname," Name cannot be empty");
+     }else{
+         onSuccess(fullname);
+     }
+	// Address
+	if(add.value.trim()===""){
+        onError(add," Address cannot be empty");
+     }else{
+         onSuccess(add);
+     }
+	// zip code
+	if(zip.value.trim()===""){
+        onError(zip," zip code cannot be empty");
+     }else{
+         onSuccess(zip);
+     }
+	
+	
+}
+
+document.querySelector("button")
+.addEventListener("click",(event)=>{
+    event.preventDefault();
+    validateInput();
+});
+
+function onSuccess(input){
+    let parent=input.parentElement;
+    let messageEle=parent.querySelector("small");
+    messageEle.style.visibility="hidden"; 
+    parent.classList.remove("error");
+    parent.classList.add("success");  
+}
+function onError(input,message){
+    let parent=input.parentElement;
+    let messageEle=parent.querySelector("small");
+    messageEle.style.visibility="visible";
+    messageEle.innerText=message;  
+    parent.classList.add("error");
+    parent.classList.remove("success");
+
+}
+
+function isValidEmail(email){
+   return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+
+
